e241d4090817c7f7067112975f2e0c669b209e36
JuraiAndou <samuel.gomes.afonso@gmail.com>
2015-11-20 17:02:23 -0300

    v1.1

---
+++ rodape.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link href="css/rodape.css" rel="stylesheet">
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+        
+    ?>
+    <footer>
+        <div>
+            <span><a>teste</a></span>
+        </div>
+    </footer>
+</body>
+</html>
