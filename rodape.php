df8a60a5da3ae299c510877ff2a564590250fd49
JuraiAndou <samuel.gomes.afonso@gmail.com>
2015-11-23 13:52:18 -0300

    v1.1
    rodape

--- rodape.php
+++ rodape.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <link href="css/rodape.css" rel="stylesheet">
     <meta charset="UTF-8">
+    <link href="css/topo.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"> </script>
+    <link href="css/rodape.css" rel="stylesheet">
 </head>
 <body>
     <?php
         
     ?>
     <footer>
-        <div>
-            <span><a>teste</a></span>
+        <div class="modal-footer">
+            <p>
+                <a>teste</a>
+            </p>
         </div>
     </footer>
 </body>
