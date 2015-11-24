805be72b8907daee7dbde0bfba7ae55f93688783
JuraiAndou <samuel.gomes.afonso@gmail.com>
2015-11-24 17:03:36 -0300

    v3.3
    rodape do sinte, ainda fantão ajustes

--- rodape.php
+++ rodape.php
@@ -7,7 +7,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"> </script>
     <link href="css/rodape.css" rel="stylesheet">
-</head>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 <body>
     <?php
         
@@ -15,11 +15,43 @@
     <footer>
         <nav id="footerMenu">
             <ul>
-                <li><img src="imagens/logoSemNome.png"></li>
+                <li><img src="imagens/logoSemNome.png" id="logoSemNome"></li>
+                <li><a id="home" href="#" class="link">Home</a></li>
+                <li><a id="empresa" href="#" class="link">A empresa</a></li>
+                <li><a id="cobertura" href="#" class="link">Cobertura</a></li>
+                <li><a id="assinatura" href="#" class="link">Assinaturas</a></li>
+                <li><a id="contato" href="#">Contato</a></li>
+                <li><a id="subLink1" class="subLink">submenu</a></li>
+                <li><a id="subLink2" class="subLink">submenu</a></li>
+                <li><a id="subLink3" class="subLink">submenu</a></li>
+
             </ul>
         </nav>
+
         <div id="abaDireitos">
-            <p class="direitos">Copyriight 2015 Connect - Alguns direitos reservados.</p>
+            <nav id="breadcrumb">
+                <ul id="BC">
+                    <li><a id="home" href="#" class="link">Home</a></li>
+                    <li><a id="empresa" href="#" class="link">A empresa</a></li>
+                    <li><a id="cobertura" href="#" class="link">Cobertura</a></li>
+                    <li><a id="assinatura" href="#" class="link">Assinaturas</a></li>
+                    <li><a id="contato" href="#">Contato</a></li>
+                    <li><a id="subLink1" class="subLink">submenu</a></li>
+                    <li><a id="subLink4" class="subLink">submenu</a></li>
+                    <li><a id="subLink5" class="subLink">submenu</a></li>
+                </ul>
+
+                <ul id="BC2">
+                    <li>
+                        <a href="#" class="link">Home</a><br>
+                        <a href="#" class="link">A empresa</a><br>
+                        <a href="#" class="link">Cobertura</a><br>
+                        <a href="#" class="link">Assinaturas</a><br>
+                        <a id="contato" href="#">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+            <p class="direitos">Copyright 2015 Connect - Alguns direitos reservados.</p>
         </div>
     </footer>
 </body>
