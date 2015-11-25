5afee26e69d82ce26abe86521e8d363749b797db
JuraiAndou <samuel.afonso.gomes@gmail.com>
2015-11-25 15:32:56 -0300

    v3.4
    rodape

--- rodape.php
+++ rodape.php
@@ -17,40 +17,21 @@
             <ul>
                 <li><img src="imagens/logoSemNome.png" id="logoSemNome"></li>
                 <li><a id="home" href="#" class="link">Home</a></li>
-                <li><a id="empresa" href="#" class="link">A empresa</a></li>
+                <li><a id="empresa" href="#" class="link">A empresa</a>
+                    <ul class="submenu">
+                        <li><a class="subLink">submenu</a></li>
+                        <li><a class="subLink">submenu</a></li>
+                        <li><a class="subLink">submenu</a></li>
+                    </ul>
+                </li>
+
                 <li><a id="cobertura" href="#" class="link">Cobertura</a></li>
                 <li><a id="assinatura" href="#" class="link">Assinaturas</a></li>
                 <li><a id="contato" href="#">Contato</a></li>
-                <li><a id="subLink1" class="subLink">submenu</a></li>
-                <li><a id="subLink2" class="subLink">submenu</a></li>
-                <li><a id="subLink3" class="subLink">submenu</a></li>
-
             </ul>
         </nav>
 
         <div id="abaDireitos">
-            <nav id="breadcrumb">
-                <ul id="BC">
-                    <li><a id="home" href="#" class="link">Home</a></li>
-                    <li><a id="empresa" href="#" class="link">A empresa</a></li>
-                    <li><a id="cobertura" href="#" class="link">Cobertura</a></li>
-                    <li><a id="assinatura" href="#" class="link">Assinaturas</a></li>
-                    <li><a id="contato" href="#">Contato</a></li>
-                    <li><a id="subLink1" class="subLink">submenu</a></li>
-                    <li><a id="subLink4" class="subLink">submenu</a></li>
-                    <li><a id="subLink5" class="subLink">submenu</a></li>
-                </ul>
-
-                <ul id="BC2">
-                    <li>
-                        <a href="#" class="link">Home</a><br>
-                        <a href="#" class="link">A empresa</a><br>
-                        <a href="#" class="link">Cobertura</a><br>
-                        <a href="#" class="link">Assinaturas</a><br>
-                        <a id="contato" href="#">Contato</a>
-                    </li>
-                </ul>
-            </nav>
             <p class="direitos">Copyright 2015 Connect - Alguns direitos reservados.</p>
         </div>
     </footer>
