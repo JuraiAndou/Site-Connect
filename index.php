77b60ce49ee0dccfa5df37b2bc840dd7a8382b70
JuraiAndou <samuel.gomes.afonso@gmail.com>
2015-12-09 13:13:17 -0300

    slide

--- index.php
+++ index.php
@@ -83,42 +83,19 @@
         </div>
     </div>
     <div class="amarela">
-        <div class="acessoRapido" style="background-image: url('imagens/fundoGrid.gif')">
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
+        <div class="acessoRapido">
+            <div class="botoes">
+                <a href="#" class="botao"></a>
+                <a href="#" class="botao"></a>
+                <a href="#" class="botao"></a>
+                <a href="#" class="botao"></a>
+                <a href="#" class="botao"></a>
+                <a href="#" class="botao"></a>
+            </div>
         </div>
     </div>
     <!--Roda pé-->
     <div id="footer">
-        <footer>
-            <nav id="footerMenu">
-                <ul>
-                    <li class="li1"><img src="imagens/logoSemNome.png" id="logoSemNome"></li>
-                    <li class="li1"><a id="home" href="#" class="link">Home</a></li>
-                    <li class="li1"><a id="empresa" href="#" class="link">A empresa</a>
-                        <ul class="submenu">
-                            <li><a class="subLink">submenu</a></li>
-                            <li><a class="subLink">submenu</a></li>
-                            <li><a class="subLink">submenu</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="li1"><a id="cobertura" href="#" class="link">Cobertura</a></li>
-                    <li class="li1"><a id="assinatura" href="#" class="link">Assinaturas</a></li>
-                    <li class="li1"><a id="contato" href="#">Contato</a></li>
-                </ul>
-            </nav>
-
-            <div id="abaDireitos">
-                <p class="direitos">Copyright 2015 Connect<span id="spanDireitos"> - Alguns direitos reservados.</span></p>
-            </div>
-        </footer>
-    </div>
-    <!--<div id="footer">
         <nav id="footerMenu">
             <ul>
                 <li class="li1"><img src="imagens/logoSemNome.png" id="logoSemNome"></li>
@@ -140,7 +117,7 @@
         <div id="abaDireitos">
             <p class="direitos">Copyright 2015 Connect<span id="spanDireitos"> - Alguns direitos reservados.</span></p>
         </div>
-    </div>-->
+    </div>
 </div>
 </body>
 </html>
