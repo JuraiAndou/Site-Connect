21597aed27cb83f10b291b351c3d99b0a1b53e9d
JuraiAndou <samuel.gomes.afonso@gmail.com>
2015-12-10 13:41:14 -0300

    velho footer novo

--- pg/rodape.php
+++ pg/rodape.php
@@ -1,24 +1,26 @@
 <div id="footer">
-    <nav id="footerMenu">
-        <ul>
-            <li id="img"><img src="imagens/logoSemNome.png" id="logoSemNome"></li>
-            <li class="li1"><a id="home" href="#" class="link">Home</a></li>
-            <li class="li1" id="liSub"><a id="empresa" href="#" class="link">A empresa</a>
-                <ul class="submenu">
-                    <li><a class="subLink">submenu</a></li>
-                    <li><a class="subLink">submenu</a></li>
-                    <li><a class="subLink">submenu</a></li>
-                </ul>
-            </li>
+    <footer>
+        <nav id="footerMenu">
+            <ul>
+                <li class="li1"><img src="imagens/logoSemNome.png" id="logoSemNome"></li>
+                <li class="li1"><a id="home" href="#" class="link">Home</a></li>
+                <li class="li1" id="liSub"><a id="empresa" href="#" class="link">A empresa</a>
+                    <ul class="submenu">
+                        <li><a class="subLink">submenu</a></li>
+                        <li><a class="subLink">submenu</a></li>
+                        <li><a class="subLink">submenu</a></li>
+                    </ul>
+                </li>
 
-            <li class="li1"><a id="cobertura" href="#" class="link">Cobertura</a></li>
-            <li class="li1"><a id="assinatura" href="#" class="link">Assinaturas</a></li>
-            <li class="li1"><a id="contato" href="#">Contato</a></li>
-        </ul>
-    </nav>
-    <div id="abaDireitos">
-        <p class="direitos">Copyright 2015 Connect<span id="spanDireitos"> - Alguns direitos reservados.</span></p>
-    </div>
+                <li class="li1"><a id="cobertura" href="#" class="link">Cobertura</a></li>
+                <li class="li1"><a id="assinatura" href="#" class="link">Assinaturas</a></li>
+                <li class="li1"><a id="contato" href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <div id="abaDireitos">
+            <p class="direitos">Copyright 2015 Connect<span id="spanDireitos"> - Alguns direitos reservados.</span></p>
+        </div>
+    </footer>
 </div>
 </body>
 </html>
